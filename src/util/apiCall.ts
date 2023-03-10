c89424c75d0025b831cdd33b9797f9ebc5c35f4a
Amy <47392011+vtheskeleton@users.noreply.github.com>
2023-03-11 01:10:14 +0200

    Return type for apiCall

--- src/util/apiCall.ts
+++ src/util/apiCall.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 import ApiResponse from "../types/ApiResponse";
 
+/**
+ * Makes an API call to the OMG.LOL API
+ * @param token
+ * @param method
+ * @param path
+ * @param body?
+ * @param additionalHeaders?
+ * @returns {Promise<ApiResponse>}
+ */
 export default async function (token: string, method: string = 'GET', path: string, body : any = undefined, additionalHeaders : any = {}) : Promise<ApiResponse> {
     const apiUri = `https://api.omg.lol${path}`
     const headers = {
