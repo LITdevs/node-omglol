10ce1b549d3955f8054ae4542c310fb5a1cbb460
Vukky <vukky@litdevs.org>
2023-03-10 23:36:47 +0100

    Add fancy comments and fix types :3

--- src/types/IAddress.ts
+++ src/types/IAddress.ts
@@ -1,32 +1,34 @@
 export default interface IAddress {
-    address: string;
-    message: string;
+    address: string; // vukky
+    message: string; // This address was registered 7 months and 1 week ago.
     registration: {
-        message: string;
-        unix_epoch_time: string;
-        iso_8601_time: string;
-        rfc_2822_time: string;
-        relative_time: string;
+        message: string; // This address was registered 7 months, 1 week and 2 days ago.
+        unix_epoch_time: number; // 1659286545
+        iso_8601_time: string; // 2022-07-31T16:55:45+00:00
+        rfc_2822_time: string; // Sun, 31 Jul 2022 16:55:45 +0000
+        relative_time: string; // 7 months, 1 week and 2 days ago
     }
     expiration: {
-        expired: boolean;
-        will_expire: boolean;
-        unix_epoch_time?: string;
-        iso_8601_time?: string;
-        rfc_2822_time?: string;
-        relative_time?: string;
+        message: string; // This address expires in 1 year, 4 months and 3 weeks.
+        expired: boolean; // false
+        will_expire: boolean; // true
+        unix_epoch_time?: number; // 1722444945
+        iso_8601_time?: string; // 2024-07-31T16:55:45+00:00
+        rfc_2822_time?: string; // Wed, 31 Jul 2024 16:55:45 +0000
+        relative_time?: string; // 1 year, 4 months and 3 weeks
     }
     preferences: {
-        include_in_directory: boolean;
-        show_on_dashboard: boolean;
-        statuslog: {
-            mastodon_posting: boolean;
+        // TODO: i dont know what the fuck is going on here but they are not booleans
+        include_in_directory?: boolean;
+        show_on_dashboard?: boolean;
+        statuslog?: {
+            mastodon_posting?: boolean;
         }
     }
 
     verification: {
-        verified: boolean;
-        message: string;
+        verified: boolean; // true
+        message: string; // This address has been verified.
     }
 
     owner: string
